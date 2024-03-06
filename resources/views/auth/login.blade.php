4c2a05283b54c8928626358987d979e3c96f7b87
IBRA-oub <brahimbourih@gmail.com>
2024-03-06 09:56:13 +0100

    login ui et backend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+            <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+            <title>{{ config('app.name', 'Laravel') }}</title>
+            
+            <!-- Fonts -->
+            <link rel="preconnect" href="https://fonts.bunny.net">
+            <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    
+            <!-- Scripts -->
+            @vite(['resources/css/register.css', 'resources/js/app.js'])
+        </head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
         </div>
+        <div class="form-container sign-in">
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                <h1>Log in</h1>
+                
+                {{-- _______email__________ --}}
+                <label for="email" class="lab">Email</label>
+                <input id="email"  type="email" name="email" :value="old('email')" required autofocus autocomplete="name">
+                <x-input-error :messages="$errors->get('email')"  style="color:red; font-size:12px; margin-top:2px;" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                {{-- ____________password________ --}}
+                <label for="Password" class="lab">Password</label>
+                <input  id="password"   type="password" name="password" required autocomplete="current-password" >
+                <x-input-error :messages="$errors->get('password')"  style="color:red; font-size:12px; margin-top:2px;"/>
+
+                
+                    @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}"> {{ __('Forgot your password?') }}</a>
+                    @endif
+                <button>{{ __('Login') }}</button>
+            </form>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
+        <div class="toggle-container">
+            <div class="toggle">
+               
+                <div class="toggle-panel toggle-right">
+                    <h1>WELCOME BACK!</h1>
+                    <p>Découvrire nouvelle evenemment et reserver votre ticket</p>
+                </div>
+            </div>
         </div>
+    </div>
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+</html>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+
+
+    
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,56 +1,5 @@
 
-    {{-- <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form> --}}
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
         <head>
@@ -91,17 +40,17 @@
                 {{-- _______fullName__________ --}}
                 <label for="fullName" class="lab">fullName</label>
                 <input id="name"  type="text" name="fullName" :value="old('name')" required autofocus autocomplete="name">
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <x-input-error :messages="$errors->get('name')" style="color:red; font-size:12px; margin-top:2px;" />
 
                 {{-- _________email__________ --}}
                 <label for="email" class="lab">email</label>
                 <input id="email"  type="email" name="email" :value="old('email')" required autocomplete="username">
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <x-input-error :messages="$errors->get('email')" style="color:red; font-size:12px; margin-top:2px;" />
 
                 {{-- ____________password________ --}}
                 <label for="Password" class="lab">Password</label>
                 <input  id="password" type="password" name="password" required autocomplete="new-password" >
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password')" style="color:red; font-size:12px; margin-top:2px;" />
 
                  <input  id="organisateur" type="hidden" name="role" value="organisateur" >
 
@@ -128,17 +77,17 @@
                 {{-- _______fullName__________ --}}
                 <label for="fullName" class="lab">fullName</label>
                 <input id="name"  type="text" name="fullName" :value="old('name')" required autofocus autocomplete="name">
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <x-input-error :messages="$errors->get('name')" style="color:red; font-size:12px; margin-top:2px;" />
 
                 {{-- _________email__________ --}}
                 <label for="email" class="lab">email</label>
                 <input id="email"  type="email" name="email" :value="old('email')" required autocomplete="username">
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <x-input-error :messages="$errors->get('email')" style="color:red; font-size:12px; margin-top:2px;" />
 
                 {{-- ____________password________ --}}
                 <label for="Password" class="lab">Password</label>
                 <input  id="password" type="password" name="password" required autocomplete="new-password" >
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password')" style="color:red; font-size:12px; margin-top:2px;" />
 
                  <input  id="organisateur" type="hidden" name="role" value="client" >
 
